a7b74fc39a7fa4bf36142529718459d1b224837f
omcodes55 <vadherom999@gmail.com>
2026-07-27 17:22:11 +0530

    MongoDB CRUD

--- databasemongoose/server.js
+++ databasemongoose/server.js
@@ -6,8 +6,8 @@ const usermodel = require("./usermodel")
 app.use(express.json());
 
 
-app.get("/",(req,res)=>{
-    res.send("<h1> go to create route </h1>");
+app.get("/", (req, res) => {
+    res.send("<h1> go to users route </h1>");
 });
 
 
@@ -19,21 +19,33 @@ app.post("/create", async (req, res) => {
         username: req.body.username,
         email: req.body.email
     });
-
-    res.send(createduser);
+    res.send("User created Successfully");
 });
 
 
+app.post("/update", async (req, res) => {   //update user using unique _id 
+    const updateUser = await usermodel.findOneAndUpdate({ _id: req.body._id }, { name: req.body.name, email: req.body.email }, { new: true })
+    res.send("User Updated Successfully");
+})
 
-app.get("/users",async (req,res)=>{
+
+
+app.post("/delete", async (req,res) => {
+    const deleteuser= await usermodel.findOneAndDelete({_id: req.body._id})
+
+    res.send(`${deleteuser.username} deleted successfully`)
+    
+})
+
+
+app.get("/users", async (req, res) => {
     const users = await usermodel.find()  //find all users
-
     res.send(users)
 })
 
 
 
 
-app.listen(3000,()=>{
+app.listen(3000, () => {
     console.log("Server are running on PORT 3000")
 });
