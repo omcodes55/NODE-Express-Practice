e68557d5b1f017e094b710633bbf34b660d2b06c
omcodes55 <vadherom999@gmail.com>
2026-07-29 12:31:16 +0530

    Update Technology

--- ejswithmongo/server.js
+++ ejswithmongo/server.js
@@ -9,10 +9,29 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 const userModel = require("./model/user")
 const usermodel = require("../databasemongoose/usermodel")
+const user = require("./model/user")
 
 
 app.get("/", (req, res) => {
     res.render("index.ejs")
+})
+
+
+app.get("/read/:id", async(req, res)=>{
+    let users = await userModel.findOne({_id:req.params.id})
+    res.render("read.ejs",{users})
+})
+
+app.get("/edit/:id", async (req, res)=>{
+    let user = await userModel.findOne({_id:req.params.id})
+    res.render("edit.ejs" ,{user})
+})
+
+app.post("/update/:id", async (req, res)=>{
+
+    let { name, email, image } = req.body;
+    await userModel.findOneAndUpdate({_id:req.params.id},{name,email,image},{new:true})
+    res.redirect("/users")
 })
 
 app.get("/users", async (req, res) => {
@@ -21,6 +40,10 @@ app.get("/users", async (req, res) => {
     res.render("usercard.ejs",{users})
 })
 
+app.get("/delete/:id", async(req,res)=>{
+    let users = await userModel.findOneAndDelete({_id:req.params.id})
+    res.redirect("/users")
+})
 
 app.post("/create", async (req, res) => {
 
@@ -32,9 +55,11 @@ app.post("/create", async (req, res) => {
         image
     })
 
+    // alert("User Created Successfully")
     res.redirect("/users")
 
 })
+
 
 
 app.listen(3000, (err) => {
