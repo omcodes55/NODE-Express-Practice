062d10db8504e4a115650945c9a137ea8b380b69
omcodes55 <vadherom999@gmail.com>
2026-08-13 17:01:17 +0530

    Multer Half Completed

--- multer/index.js
+++ multer/index.js
@@ -1,6 +1,10 @@
 const express = require("express")
 const app = express()
 const path = require("path")
+const multer = require("multer")
+const cypto = require("crypto")
+const { throws } = require("assert")
+
 
 app.set("view engine","ejs")
 app.use(express.json())
@@ -8,9 +12,30 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.static(path.join(__dirname, "public")));
 
 
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './public/images/uploads')
+  },
+  filename: function (req, file, cb) {
+    cypto.randomBytes(12,(err,bytes)=>{
+        const fn = bytes.toString('hex') + path.extname(file.originalname)
+        cb(null, fn)    
+    })
+  }
+})
+const upload = multer({ storage: storage })
+
+
 app.get("/",(req,res)=>{
     res.render("multer")
 })
+
+app.post("/upload", upload.single("image"), (req, res)=>{
+    
+    console.log(req.file);
+    res.send("File uploaded successfully!");
+});
+
 
 
 app.listen(5000,(err)=>{
